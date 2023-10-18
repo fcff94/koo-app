ef7700aa8c866567958561e73cc820443332539f
fcff94 <fati_carol@hotmail.com>
2023-10-18 22:06:40 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.scss'
+import { CardComponent } from './components/card/Card'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <CardComponent />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/card/Card.tsx
@@ -0,0 +1,29 @@
+export function CardComponent() {
+    return (
+        <>
+        <div className="cardWrapper">
+            <div className="cardHeader">
+                <div>
+                    
+                </div>
+            </div>
+
+            <div className="cardText">
+
+            </div>
+
+            <div className="cardMedia">
+
+            </div>
+
+            <div className="cardForm">
+
+            </div>
+
+            <div className="cardActions">
+
+            </div>
+        </div>
+        </>
+    );
+}
