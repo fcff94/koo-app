761667b7d5ac3b8b1a8181aafc84f5a8a0b93c2a
Rafael Ferreira <andersonrafaelferreira@gmail.com>
2023-11-07 17:49:34 -0300

    applying axios | refactor date | mb: 20px

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,49 @@ import './App.scss';
 import { CardComponent } from './components/card/Card';
 import profilePhoto from '@/assets/img/profile_photo.jpeg';
 
+import { api } from "./services/api";
+import { useState, useEffect } from "react";
+
+export type PostType = {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+};
+
 function App() {
   // const [count, setCount] = useState(0)
-  const text = `
-  Welcome to the Koo family 🙂
-  1. Follow people you like by clicking on the '+ Follow' button to see their posts in your feed.
-  2. Share your thoughts with others and gain a following.
-  Happy Koo to you!
-  `;
+
+  const [list, setList] = useState<PostType[]>([]);
+
+  async function loadPosts() {
+    const result = await api.get("users/1/posts");
+    setList(result.data);
+  }
+
+  useEffect(() => {
+    loadPosts();
+  }, []);
+
+  // const text = `
+  // Welcome to the Koo family 🙂
+  // 1. Follow people you like by clicking on the '+ Follow' button to see their posts in your feed.
+  // 2. Share your thoughts with others and gain a following.
+  // Happy Koo to you!
+  // `;
 
   return (
     <>
-      <CardComponent
-        userImg={profilePhoto}
-        displayName={'Koo English'}
-        userName={'@kooenglish'}
-        date={'21/07/2023'}
-        text={text}
-      />
+      {list.map((tweet, index) => (
+        <CardComponent
+          key={index}
+          userImg={profilePhoto}
+          displayName={`Koo English ${tweet.userId}`}
+          userName={"@kooenglish"}
+          date={String(`${new Date().getDate()}/${new Date().getMonth()+1}/${new Date().getFullYear()}`)}
+          text={tweet.title}
+        />
+      ))}
     </>
   );
 }
